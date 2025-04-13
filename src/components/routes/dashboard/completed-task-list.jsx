f9fe9f5fa6b47f47c024ba264132aa8cc4e97e01
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-13 17:33:25 +0530

    Found modified or deleted file,  src/components/routes/dashboard/completed-task-list.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/components/routes/dashboard/completed-task-list.jsx
+++ src/components/routes/dashboard/completed-task-list.jsx
@@ -1,3 +1,39 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 export default function CompletedTaskList() {
-  return <div>CompletedTaskList</div>;
+  const [completedTasks, setCompletedTasks] = useState([]);
+
+  useEffect(() => {
+    setCompletedTasks([]);
+  }, []);
+
+  if (!completedTasks || completedTasks.length == 0) {
+    return (
+      <main class="dashboard-tasks mt-5">
+        <h2 class="fs-4 fw-100 mx-3">Your Tasks</h2>
+        <div class="fw-100 no-scrollbar ms-5 me-1 my-3" id="dashboardTasks">
+          <p class="">Track some tasks to view them here.</p>
+          <p class="fw-300">
+            Click on
+            <a href="/tasks" className="">
+              <img
+                src="/src/assets/img/all-tasks-icon.svg"
+                alt="All tasks icon"
+                class="logo-in-article"
+              />
+            </a>
+            to view all taks or Search for tasks
+            <a href="/tasks?search-term=#" className="">
+              <img
+                src="/src/assets/img/find-task-icon.svg"
+                alt="All tasks icon"
+                class="logo-in-article"
+              />
+            </a>
+          </p>
+        </div>
+      </main>
+    );
+  }
 }
