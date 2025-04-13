f9a7131c381ae3448d67dd1027d39f9db803517c
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-13 16:50:54 +0530

    Found modified or deleted file,  src/main.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,23 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/dashboard/dashboard";
 import "./styles/app.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./"
+import "./styles/defaults.css";
+import Settings from "./components/settings/settings";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/profile/settings" element={<Settings />} />
+      <Route path="/guide" element={<Dashboard />} />
+      <Route path="/tasks" element={<Dashboard />} />
+      <Route path="/profile/tasks/todo" element={<Dashboard />} />
+      <Route path="/profile/tasks/removed" element={<Dashboard />} />
+      <Route path="/profile/tasks/completed" element={<Dashboard />} />
+      <Route path="/admin" element={<Dashboard />} />
+      <Route path="/contribute" element={<Dashboard />} />
+      <Route path="/support" element={<Dashboard />} />
+      <Route path="/about" element={<Dashboard />} />
     </Routes>
   </BrowserRouter>
 );
