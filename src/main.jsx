50c864f0dafe7d76c415acdfcfffed353607d6b9
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-13 15:53:22 +0530

    Found modified or deleted file,  src/main.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/dashboard/dashboard";
+import "./styles/app.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./"
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
