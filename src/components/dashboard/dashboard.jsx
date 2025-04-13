98c1746f2f3e71cb121d85fcce80e33faa975f9b
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-13 16:50:53 +0530

    Found modified or deleted file,  src/components/dashboard/dashboard.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,3 +1,17 @@
+import { useState } from "react";
+import LoadingScreen from "../common/loading-screen";
+import { useEffect } from "react";
+import NavBar from "./nav-bar";
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const [show, setShow] = useState(true);
+  useEffect(() => {
+    setShow(false);
+  }, []);
+  return (
+    <div className="bg-dark text-light no-scrollbar">
+      <LoadingScreen show={show} />
+      <NavBar />
+    </div>
+  );
 }
