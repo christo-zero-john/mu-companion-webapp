21a26c73bb6bd12c4b3abb60792aa293bf314c9d
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-28 12:48:25 +0530

    Update footer-menu.jsx

--- src/components/routes/dashboard/footer-menu.jsx
+++ src/components/routes/dashboard/footer-menu.jsx
@@ -1,41 +1,44 @@
 import React from "react";
+import MenuActions from "../../../modules/User";
 
 export default function FooterMenu() {
   return (
     <footer class="sticky-bottom menu d-flex flex-row justify-content-around align-items-center p-2">
-      <img
-        onClick={()=>MenuAcitions.printAllTasks()}
-        src="/assets/img/all-tasks-icon.svg"
-        alt="View all tasks"
-        class="menu-icon"
-      />
-
-      <img
-        onClick={()=>MenuAcitions.findTask()}
-        src="/assets/img/find-task-icon.svg"
-        alt="Find tasks"
-        class="menu-icon"
-      />
-
-      <img
-        onClick={()=>MenuAcitions.printTrackedTasks()}
-        src="/assets/img/tracked-task-icon.svg"
-        alt="View Tracked Tasks"
-        class="menu-icon"
-      />
-
-      <img
-        onClick={()=>MenuAcitions.printCompletedTasks()}
-        src="/assets/img/completed-task-icon.svg"
-        alt="View completed Tasks"
-        class="menu-icon"
-      />
-
-      <a
-        href="https://github.com/christo-zero-john/mu-companion-webapp"
-      >
+      <a href="/tasks" className="">
         <img
-          src="/assets/img/github.svg"
+          src="/src/assets/img/all-tasks-icon.svg"
+          alt="View all tasks"
+          class="menu-icon"
+        />
+      </a>
+
+      <a href="/tasks?search-term=#">
+        <img
+          src="/src/assets/img/find-task-icon.svg"
+          alt="Find tasks"
+          class="menu-icon"
+        />
+      </a>
+
+      <a href="/profile/tasks/todo">
+        <img
+          src="/src/assets/img/tracked-task-icon.svg"
+          alt="View Tracked Tasks"
+          class="menu-icon"
+        />
+      </a>
+
+      <a href="/profile/tasks/completed">
+        <img
+          src="/src/assets/img/completed-task-icon.svg"
+          alt="View completed Tasks"
+          class="menu-icon"
+        />
+      </a>
+
+      <a href="https://github.com/christo-zero-john/mu-companion-webapp">
+        <img
+          src="/src/assets/img/github.svg"
           alt="Goto Github repository"
           class="menu-icon"
         />
