4a68b5541a5750783eb9fdca6b046511d92c2d1a
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-28 12:48:31 +0530

    Update tasks.jsx

--- src/components/routes/tasks/tasks.jsx
+++ src/components/routes/tasks/tasks.jsx
@@ -1,3 +1,22 @@
+import { useState } from "react";
+import TaskItem from "./task-item";
+
+/**
+ * Route to print all tasks
+ */
 export default function Tasks() {
-  return <div>Tasks</div>;
+  const [tasks, setTasks] = useState([]);
+
+  tasks.length == 0 && <p className="">Loading...</p>;
+  return (
+    <div className="bg-dark text-light">
+      <p className="">Tasks Found: {tasks.length}</p>
+      <TaskItem />
+      <div className="">
+        {tasks.map((task) => (
+          <TaskItem task={task} key={task.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
