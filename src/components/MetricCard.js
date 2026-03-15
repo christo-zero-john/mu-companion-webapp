0dbb8bceae309ab3934593642e2f303eb5d88d58
christo-zero-john <10.christojohn@gmail.com>
2026-03-15 14:45:46 +0530

    Found modified or deleted file,  src/components/MetricCard.js. Committing Via Global Bash Script:git-status-automated.bat

--- src/components/MetricCard.js
+++ src/components/MetricCard.js
@@ -2,18 +2,18 @@ import Icon from "./Icon";
 
 export default function MetricCard({ icon, iconBgColor, value, label }) {
   return (
-    <div className="flex-1 min-w-[105px] bg-panel rounded-xl p-4 flex flex-col items-start justify-between gap-6 border border-border">
+    <div className="flex-1 min-w-[105px] bg-[#1E1E20] rounded-xl p-5 flex flex-col items-center justify-center gap-4 border border-border">
       <div
-        className={`w-10 h-10 rounded-lg flex items-center justify-center ${iconBgColor}`}
+        className={`w-12 h-12 rounded-lg flex items-center justify-center ${iconBgColor}`}
       >
-        <Icon name={icon} size={20} />
+        <Icon name={icon} size={24} />
       </div>
-      <div>
-        <div className="text-xl font-bold text-white leading-none">{value}</div>
-        {label && (
-          <div className="text-xs text-text-secondary mt-1">{label}</div>
-        )}
-      </div>
+      <div className="text-2xl font-bold text-white leading-none">{value}</div>
+      {label && (
+        <div className="text-xs text-text-secondary -mt-2 font-medium">
+          {label}
+        </div>
+      )}
     </div>
   );
 }
