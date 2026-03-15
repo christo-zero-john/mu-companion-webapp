1ad4fac75e860954b658c9f9f41ab71c3af98a66
christo-zero-john <10.christojohn@gmail.com>
2026-03-15 14:45:47 +0530

    Found modified or deleted file,  src/components/TaskCard.js. Committing Via Global Bash Script:git-status-automated.bat

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,19 +1,23 @@
 import Icon from "./Icon";
 
-export default function TaskCard({ title, hashtag, points }) {
+export default function TaskCard({ title, hashtag, points, isOdd }) {
   return (
-    <div className="w-full border-b border-border flex items-center justify-between py-4 group">
+    <div
+      className={`w-full flex items-center justify-between p-4 transition-colors ${isOdd ? "bg-card-odd" : "bg-card-even"}`}
+    >
       <div className="flex flex-col gap-1">
-        <h3 className="text-white font-bold text-sm group-hover:text-primary transition-colors cursor-pointer">
-          {title}
-        </h3>
-        <span className="text-text-secondary text-xs">{hashtag}</span>
-      </div>
-      <div className="flex items-center gap-1.5 bg-panel px-3 py-1.5 rounded-lg border border-border">
-        <Icon name="karma-points" size={16} />
-        <span className="text-white font-bold text-sm tracking-wide">
-          {points}
+        <h3 className="text-white font-bold text-sm">{title}</h3>
+        <span className="text-text-secondary text-xs font-medium">
+          {hashtag}
         </span>
+      </div>
+      <div className="flex items-center h-full">
+        <div className="flex items-center gap-1.5 bg-badge-bg p-3 aspect-square rounded-lg border border-border">
+          <Icon name="karma-points" size={16} />
+          <span className="text-white font-bold text-sm tracking-wide">
+            {points}
+          </span>
+        </div>
       </div>
     </div>
   );
